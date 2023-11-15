7e45937cbd2b4cb71ddc628d8fe93ff1fbbb3635
Arturo <arthy250@gmail.com>
2023-11-15 01:53:46 -0600

    consultar API para blogs

--- src/js/main.js
+++ src/js/main.js
@@ -12,5 +12,28 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	});
 
+	// Consultar API
+
+	var url = 'https://jsonplaceholder.typicode.com/posts';
+	
+	fetch(url).then(response => {
+		return response.json();
+	})
+	.then(data =>  {
+		data = data.filter(entry => entry.id < 4).slice(0, 100);
+		data.forEach(post => {
+			const postData = 
+			`<li class="entrada p-8 rounded-lg">
+				<h4>${post.title}</h4>
+				<p class='mb-3'>${post.body}</p>
+				<a href="#">Learn more</a>
+			</li>`
+			;
+			document.getElementById('blogPosts').insertAdjacentHTML('beforeend', postData);
+		})
+	}).catch(function(err) {
+		console.log('No data error', err);
+	});
+
 }, false);
 
