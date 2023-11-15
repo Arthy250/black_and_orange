b139bd1500540e3246bb6407fd89d0aae5dd21d1
Arturo <arthy250@gmail.com>
2023-11-14 18:57:09 -0600

    crear header

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	// Show mobile menu
+	let burguerButton = document.getElementById('burguerMenu');
+	var burguerMenu = document.getElementById("mobileMenu");
+	console.log(burguerMenu)
+	
+	burguerButton.addEventListener("click", () => {
+
+		if (burguerMenu.classList.contains("visible")) {
+			burguerMenu.classList.remove("visible");
+			// do something here if the class exists
+		} else {
+			burguerMenu.classList.add("visible");
+			// do something else here if the class does not exist
+		}
+	});
+
+}, false);
+
