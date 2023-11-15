2fac5649fe20e8500e94c690c42d5608dcc68279
Arturo <arthy250@gmail.com>
2023-11-14 21:18:22 -0600

    seccion: grafica

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
 	var burguerMenu = document.getElementById("mobileMenu");
 	
 	burguerButton.addEventListener("click", () => {
-
 		if (burguerMenu.classList.contains("visible")) {
 			burguerMenu.classList.remove("visible");
-			// do something here if the class exists
 		} else {
 			burguerMenu.classList.add("visible");
-			// do something else here if the class does not exist
 		}
 	});
 
